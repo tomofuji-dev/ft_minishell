96c1f90a63702e815e3d93be14b9de50f264458a
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-01 13:58:19 +0900

    add check_argc

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -1,18 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:17:50 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/01 13:18:16 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/01 13:57:00 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "pipex.h"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	printf("hello, world!");
+	(void) argv;
+	if (check_argc(argc) == false)
+		return (1);
+	return (0);
 }
