ede5f7d863748f29579e8ce066a4edf82061e14a
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-13 13:15:22 +0900

    remove comments

--- src/test_lexer.c
+++ src/test_lexer.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:47:24 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/11 16:27:02 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/13 13:14:22 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,53 +50,3 @@ int	main(void)
 	test_ms_lexer_endpos();
 	return (0);
 }
-
-// 
-// size_t	ms_lexer_endpos_delim(char *line)
-// {
-// 	int		ch;
-// 	size_t	pos;
-
-// 	if (!ft_strchr(&CHRS_DELIM[1], *line))
-// 		return (0);
-// 	ch = *line;
-// 	pos = 0;
-// 	while (*(line + pos) == ch)
-// 		pos++;
-// 	return (pos);
-// }
-
-// size_t	ms_lexer_endpos_quoted(char *line)
-// {
-// 	int		ch;
-// 	char	*line_end;
-
-// 	if (!ft_strchr(CHRS_QUOTE, *line))
-// 		return (0);
-// 	ch = *line;
-// 	line_end = ft_strchr(line + 1, ch);
-// 	if (!line_end)
-// 		return (1);
-// 	return (line_end - line);
-// }
-
-// size_t	ms_lexer_endpos(char *line)
-// {
-// 	size_t	pos;
-
-// 	pos = 0;
-// 	if (line == NULL)
-// 		return (0);
-// 	else if (ft_strchr(&CHRS_DELIM[1], *(line + pos)))
-// 		return (ms_lexer_endpos_delim(line + pos));
-// 	while (*(line + pos))
-// 	{
-// 		if (ft_strchr(CHRS_QUOTE, *(line + pos)))
-// 			pos += ms_lexer_endpos_quoted(line + pos);
-// 		else if (ft_strchr(CHRS_DELIM, *(line + pos)))
-// 			return (pos);
-// 		else
-// 			pos++;
-// 	}
-// 	return (pos);
-// }
--- src/test_lexer_tokensize.c
+++ src/test_lexer_tokensize.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:47:24 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/11 16:25:58 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/13 13:14:14 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,75 +25,3 @@ int	main(int argc, char *argv[])
 	printf("%zu\n", ms_lexer_tokensize(argv[1]));
 	return (0);
 }
-
-
-// size_t	ms_lexer_tokenlen_delim(char *line)
-// {
-// 	int		ch;
-// 	size_t	pos;
-
-// 	if (!ft_strchr(&CHRS_DELIM[1], *line))
-// 		return (0);
-// 	ch = *line;
-// 	pos = 0;
-// 	while (*(line + pos) == ch)
-// 		pos++;
-// 	return (pos);
-// }
-
-// size_t	ms_lexer_tokenlen_quoted(char *line)
-// {
-// 	int		ch;
-// 	char	*line_end;
-
-// 	if (!ft_strchr(CHRS_QUOTE, *line))
-// 		return (0);
-// 	ch = *line;
-// 	line_end = ft_strchr(line + 1, ch);
-// 	if (!line_end)
-// 		return (1);
-// 	return (line_end - line + 1);
-// }
-
-// size_t	ms_lexer_tokenlen(char *line)
-// {
-// 	size_t	pos;
-
-// 	pos = 0;
-// 	if (line == NULL)
-// 		return (0);
-// 	else if (ft_strchr(&CHRS_DELIM[1], *(line + pos)))
-// 		return (ms_lexer_tokenlen_delim(line + pos));
-// 	while (*(line + pos))
-// 	{
-// 		if (ft_strchr(CHRS_QUOTE, *(line + pos)))
-// 			pos += ms_lexer_tokenlen_quoted(line + pos);
-// 		else if (ft_strchr(CHRS_DELIM, *(line + pos)))
-// 			return (pos);
-// 		else
-// 			pos++;
-// 	}
-// 	return (pos);
-// }
-
-// size_t	ms_lexer_tokensize(char *line)
-// {
-// 	size_t	size;
-// 	size_t	pos;
-// 	size_t	len;
-
-// 	if (line == NULL)
-// 		return (0);
-// 	size = 0;
-// 	pos = 0;
-// 	while (*(line + pos) != '\0')
-// 	{
-// 		while (*(line + pos) == CHRS_DELIM[0])
-// 			pos++;
-// 		len = ms_lexer_tokenlen(line + pos);
-// 		if (len > 0)
-// 			size++;
-// 		pos += len;
-// 	}
-// 	return (size);
-// }
