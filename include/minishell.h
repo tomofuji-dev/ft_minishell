87649549e2c35931f42e83ecffafac427e4a3b85
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-14 15:18:13 +0900

    modify parser_cmdnew_input

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 13:51:23 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/14 14:55:41 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/14 15:17:45 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,11 +88,12 @@ size_t	ms_lexer_tokenlen_plain(char *line);
 
 char	*ms_lexer_string(char *line);
 
-t_cmd	*ms_parser(t_token* token)
+t_cmd	*ms_parser(t_token *token);
 t_cmd	*ms_parser_cmdnew(t_token *token, size_t *idx);
 char	**ms_parser_cmdnew_arg(t_token *token, size_t i_token);
 size_t	ms_parser_cmdnew_arg_size(t_token *token, size_t idx);
 size_t	ms_parser_cmdnew_fdsize(t_token *token, size_t idx, int flag);
+t_fd	*ms_parser_cmdnew_input(t_token *token, size_t i_token);
 
 bool	ms_isenvchar(int c);
 char	*ms_search_env(char *env_key);
--- src/ms_parser_cmdnew_input.c
+++ src/ms_parser_cmdnew_input.c
@@ -3,14 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ms_parser_cmdnew_input.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
+/*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 14:28:00 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/14 14:12:15 by Yoshihiro K      ###   ########.fr       */
+/*   Updated: 2022/12/14 15:16:47 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+t_fd		*ms_parser_cmdnew_input(t_token *token, size_t i_token);
+static int	get_heredoc_pipe(const char *eof);
+static void	get_heredoc_txt(char *eof, int fd);
 
 t_fd	*ms_parser_cmdnew_input(t_token *token, size_t i_token)
 {
@@ -18,14 +22,14 @@ t_fd	*ms_parser_cmdnew_input(t_token *token, size_t i_token)
 	ssize_t	size;
 	ssize_t	i_input;
 
-	size = ms_parser_cmdnew_fdsize(*token, i_token, FLAG_IN);
-	if (size == SIZE_INVAL)
+	size = ms_parser_cmdnew_fdsize(token, i_token, FLAG_IN);
+	if (size == SIZE_INVALID)
 		return (NULL);
-	input = (t_fd *)malloc((size + 1)sizeof(t_fd));
+	input = (t_fd *)malloc((size + 1) * sizeof(t_fd));
 	if (input == NULL)
 		retun (NULL);
 	i_input = 0;
-	while (token[i_token] != NULL && token[i_token].flag != FLAG_PIPE)
+	while (token[i_token].str != NULL && token[i_token].flag != FLAG_PIPE)
 	{
 		if (token[i_token].flag & FLAG_IN == FLAG_IN)
 		{
@@ -33,11 +37,11 @@ t_fd	*ms_parser_cmdnew_input(t_token *token, size_t i_token)
 				input[i_input].fd = open(token[++i_token].str, O_RDONLY);
 			else if (token[i_token].flag == FLAG_HEREDOC)
 				input[i_input].fd = get_heredoc_pipe(token[++i_token].str);
-			input[i_input++].path == token[i_token]->str;
+			input[i_input++].path == token[i_token].str;
 		}
 		i_token++;
 	}
-	input[size] == NULL;
+	input[size].path == NULL;
 	return (input);
 }
 
@@ -46,7 +50,21 @@ static int	get_heredoc_pipe(const char *eof)
 	int	fd[2];
 
 	pipe(fd);
-	get_heredoc_txt(eof, fd[0]);
-	close(fd[0]);
-	return (fd[1]);
+	get_heredoc_txt(eof, fd[1]);
+	close(fd[1]);
+	return (fd[0]);
+}
+
+static void	get_heredoc_txt(char *eof, int fd)
+{
+	char	*buf;
+
+	while (1)
+	{
+		buf = readline("> ");
+		if (ft_strncmp(buf, eof, INT_MAX) == 0)
+			return (free(buf));
+		ft_putendl_fd(buf, fd);
+		free(buf);
+	}
 }
