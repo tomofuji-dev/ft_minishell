6a3cca13d1251bdda678fa0831bd58c219a7d191
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2022-12-19 15:43:36 +0900

    Modify cd, pwd, add exit

--- src/ms_builtin_cd.c
+++ src/ms_builtin_cd.c
@@ -6,34 +6,48 @@
 /*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:08:21 by t.fuji            #+#    #+#             */
-/*   Updated: 2022/12/19 14:03:44 by Yoshihiro K      ###   ########.fr       */
+/*   Updated: 2022/12/19 15:30:15 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-/*#include "minishell.h"
+#include <limits.h>
+#include "minishell.h"
 
-void	ms_builtin_cd(t_cmd *cmd)
+#define ENV_HOME	"HOME"
+#define CHR_HOME	'/'
+#define CHR_HOME	'~'
+
+void	ms_builtin_cd(char	*argv[])
 {
-	char	**arg;
+	char	path[PATH_MAX + 1];
 
-	if (cmd == NULL)
+	if (argv == NULL)
 		return ;
-	if (cmd->arg[1] == NULL)
+	if (argv[1] == NULL)
+		chdir(getenv(ENV_HOME));
+	else if ft_strchr(argv[1][0] == CHR_HOME \
+		&& (argv[1][1] == CHR_DIR || argv[1][1] == '\0'))
 	{
-
+		ft_strlcpy(path, getenv(ENV_HOME), PATH_MAX + 1);
+		ft_strlcat(path, &argv[1][1], PATH_MAX + 1);
 	}
 	else
-	{
-		chdir(cmd->arg[1]);
-	}
+		ft_strlcpy(path, argv[1], PATH_MAX + 1);
+	chdir(path);
 }
-*/
+
+/*
 int	main()
 {
+	char	*buf;
+
+	printf("CWD: %s\n", getcwd(buf, PATH_MAX));
 	printf("HOME: %s\n", getenv("HOME"));
-	chdir(getenv("HOME"));
+	chdir("/home");
+	printf("CWD: %s\n", getcwd(buf, PATH_MAX));
 	printf("PWD: %s\n", getenv("PWD"));
 }
+*/
--- src/ms_builtin_exit.c
+++ src/ms_builtin_exit.c
@@ -1,40 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ms_builtin_pwd.c                                   :+:      :+:    :+:   */
+/*   ms_builtin_exit.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:08:21 by t.fuji            #+#    #+#             */
-/*   Updated: 2022/12/19 14:44:46 by Yoshihiro K      ###   ########.fr       */
+/*   Updated: 2022/12/19 15:39:26 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ms_builtin_pwd(char *argv[])
+#define MSG_EXIT	"exit"
+
+void	ms_builtin_exit(char *argv[])
 {
-	char	*buf;
+	int	status;
 
-	printf("%s\n", getcwd(buf, PATH_MAX));
+	printf("%s\n", MSG_EXIT);
+	if (argv[1] == NULL)
+		status = g_env->status;
+	else
+		ft_atoi(argv[1]);
+	exit(status);
 }
-
-/*
-void	ms_builtin_pwd(t_cmd *cmd)
-{
-	char	**arg;
-
-	if (cmd->arg[1] == NULL)
-	{
-		arg = (char **)malloc(3 * sizeof(char *));
-		if (arg == NULL)
-			return ;
-		arg[1] = (char *)malloc((getenv(ENV_HOME) + ) * sizeof(char));
-		if (arg == NULL)
-			return ;
-		arg[0] = cmd->arg[0];
-		free (cmd->arg);
-		cmd->arg = arg;
-	}
-}
-*/
