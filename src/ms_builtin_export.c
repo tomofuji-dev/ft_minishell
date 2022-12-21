2a7973e3df38a677d3b5891e0d33702e79a7a339
tfuji <tomofuji.dev@gmail.com>
2022-12-21 14:33:49 +0900

    modify init to dup environ by malloc

--- src/ms_builtin_export.c
+++ src/ms_builtin_export.c
@@ -6,45 +6,33 @@
 /*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 12:39:51 by t.fuji            #+#    #+#             */
-/*   Updated: 2022/12/21 12:57:39 by t.fuji           ###   ########.fr       */
+/*   Updated: 2022/12/21 14:29:02 by t.fuji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	ms_builtin_export(char *argv[])
+int	ms_count_valid_env(char *argv[])
 {
 	char	*equal_pos;
 
 	if (argv == NULL)
-		return (false);
-	if (ms_count_str(argv) != 2)
-		return (false);
+		return (-1);
+	if (ms_count_str(argv) < 1)
+		return (-1);
 	equal_pos = ft_strchr(argv[1], '=');
 	if (equal_pos == NULL || equal_pos == &argv[1])
 		return (false);
-}
-
-size_t	ms_count_str(char *str_lst[])
-{
-	size_t	count;
-
-	if (str_lst == NULL)
-		return (0);
-	count = 0;
-	while (str_lst[i] != NULL)
-		count++;
-	return (count);
+	return (true);
 }
 
 int	ms_builtin_export(char *argv[])
 {
-	extern char	**environ;
 	char		**new_environ;
 	size_t		env_size;
 	size_t		i;
 
-	if (ms_is_valid_env(argv) == false)
+	if (ms_count_valid_env(argv) < 0)
 		return (-1);
 	env_size = ms_env_size(environ);
 	new_environ = malloc(sizeof(char *) * (env_size + 1 + 1));
--- src/ms_init.c
+++ src/ms_init.c
@@ -3,21 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   ms_init.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 16:14:42 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/20 14:15:20 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/21 14:33:05 by t.fuji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_shell	g_shell;
+t_shell		g_shell;
+extern char	**environ;
+
+void	init_global(void);
+size_t	ms_count_str(char *str_lst[]);
+void	ms_strlst_cpy(char **dest, char **src);
+
 
 void	init_global(void)
 {
-	extern char	**environ;
+	size_t	env_count;
 
-	g_shell->environ = environ;
+	env_count = ms_count_str(environ);
+	g_shell->environ = malloc(sizeof(char *) * (env_count + 1));
+	if (g_shell->environ)
+		exit(EXIT_FAILURE);
+	ms_strlst_cpy(g_shell->environ, environ);
 	g_shell->status = 0;
+}
+
+size_t	ms_count_str(char *str_lst[])
+{
+	size_t	count;
+
+	if (str_lst == NULL)
+		return (0);
+	count = 0;
+	while (str_lst[i] != NULL)
+		count++;
+	return (count);
+}
+
+void	ms_strlst_cpy(char **dest, char **src)
+{
+	size_t	i;
+
+	if (dest == NULL || src == NULL)
+		return ;
+	i = 0;
+	while (src[i] != NULL)
+	{
+		dest[i] = ft_strdup(src[i]);
+		if (dest[i] == NULL)
+			exit(EXIT_FAILURE);
+		i++;
+	}
+	dest[i] = NULL;
+	return ;
 }
