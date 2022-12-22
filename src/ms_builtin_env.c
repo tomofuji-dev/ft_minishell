b78245d17316bee2d6e2d8222979d2ac12442a38
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-22 14:58:44 +0900

    modify ms_builtin_env.c

--- src/ms_builtin_env.c
+++ src/ms_builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:39:36 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/20 11:51:28 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:58:12 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 int	ms_builtin_env(char *argv[])
 {
-	extern char	**environ;
 	size_t		i;
 
 	if (argv == NULL)
 		return (0);
 	i = 0;
-	while (environ[i] != NULL)
-		ft_putendl_fd(environ[i++], STDOUT);
+	while (g_shell.environ[i] != NULL)
+		ft_putendl_fd(g_shell.environ[i], STDOUT);
 	return (0);
 }
--- src/ms_init.c
+++ src/ms_init.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 16:14:42 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/22 14:45:23 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:57:44 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,11 @@ void	init_global(char *envp[])
 {
 	size_t	env_count;
 
-	env_count = ms_strlst_count(envp);
-	g_shell.environ = ms_tlist_from_strlst(envp);
+	g_shell.environ = ms_tlst_from_strlst(envp);
 	g_shell.status = 0;
 }
 
-t_list	*ms_tlist_from_strlst(char *envp[])
+t_list	*ms_tlst_from_strlst(char *envp[])
 {
 	t_list	head;
 	t_list	*cur;
@@ -40,7 +39,7 @@ t_list	*ms_tlist_from_strlst(char *envp[])
 		content = ft_strdup(envp[i]);
 		if (content == NULL)
 			exit(EXIT_FAILURE);
-		cur->next = ft_listnew(content);
+		cur->next = ft_lstnew(content);
 		if (cur->next == NULL)
 			exit(EXIT_FAILURE);
 		cur = cur->next;
