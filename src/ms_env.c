b64584cf78bc22c73f48d7f11826c1c1a705f83f
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-22 15:04:36 +0900

    modify ms_env

--- src/ms_env.c
+++ src/ms_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_env.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 11:50:25 by t.fuji            #+#    #+#             */
-/*   Updated: 2022/12/22 11:54:18 by t.fuji           ###   ########.fr       */
+/*   Updated: 2022/12/22 15:03:34 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ms_getenv_line(char *env_key)
 {
-	size_t	i;
+	t_list	*cur;
 
-	i = 0;
-	while (g_shell.environ[i] != NULL)
+	cur = g_shell.environ;
+	while (cur != NULL)
 	{
-		if (ft_strncmp(g_shell.environ[i], env_key, ft_strlen(env_key)) == 0)
-			return (&g_shell.environ[i]);
-		i++;
+		if (ft_strncmp(cur->environ[i], env_key, ft_strlen(env_key)) == 0)
+			return (cur->environ);
+		cur->cur->next;
 	}
 	return (NULL);
 }
