437911865326f6ef8ae9c45bd3aac110a80d5460
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-22 14:48:34 +0900

    src/ms_init.c

--- src/ms_init.c
+++ src/ms_init.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/11 16:14:42 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/22 14:21:24 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:45:23 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,29 @@ void	init_global(char *envp[])
 	size_t	env_count;
 
 	env_count = ms_strlst_count(envp);
-	g_shell.environ = malloc(sizeof(char *) * (env_count + 1));
-	if (g_shell.environ == NULL)
-		ms_strlst_cpy(g_shell.environ, envp);
+	g_shell.environ = ms_tlist_from_strlst(envp);
 	g_shell.status = 0;
+}
+
+t_list	*ms_tlist_from_strlst(char *envp[])
+{
+	t_list	head;
+	t_list	*cur;
+	char	*content;
+	size_t	i;
+
+	head.next = NULL;
+	cur = &head;
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		content = ft_strdup(envp[i]);
+		if (content == NULL)
+			exit(EXIT_FAILURE);
+		cur->next = ft_listnew(content);
+		if (cur->next == NULL)
+			exit(EXIT_FAILURE);
+		cur = cur->next;
+	}
+	return (head->next);
 }
