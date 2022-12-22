cf63a5cce41187bdccfbcaeec3dcfe6195a6e584
Tomoya Fujiwara <tomofuji.dev@gmail.com>
2022-12-22 15:00:47 +0900

    modify g_shell

--- src/ms_builtin_env.c
+++ src/ms_builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:39:36 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/22 14:58:12 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/22 14:59:59 by tfujiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	ms_builtin_env(char *argv[])
 {
-	size_t		i;
+	t_list	*cur;
 
 	if (argv == NULL)
 		return (0);
-	i = 0;
-	while (g_shell.environ[i] != NULL)
+	cur = g_shell.environ;
+	while (g_shell.environ != NULL)
 		ft_putendl_fd(g_shell.environ[i], STDOUT);
 	return (0);
 }
