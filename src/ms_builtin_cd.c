ca5d4530b53338730f15df6583201c9d3b33380a
Yoshihiro Kosaka <ykosaka@student.42tokyo.jp>
2022-12-26 16:03:03 +0900

    Modify cd (ENV_PWD in setpath_relative)

--- src/ms_builtin_cd.c
+++ src/ms_builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 14:08:21 by t.fuji            #+#    #+#             */
-/*   Updated: 2022/12/26 15:21:40 by Yoshihiro K      ###   ########.fr       */
+/*   Updated: 2022/12/26 15:53:56 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ static void	ms_builtin_cd_setpath_absolute(char	*path, char	*arg)
 
 static void	ms_builtin_cd_setpath_relative(char	*path, char	*arg)
 {
-	ft_strlcpy(path, ms_getenv_val(ENV_HOME), PATH_MAX + 1);
+	ft_strlcpy(path, ms_getenv_val(ENV_PWD), PATH_MAX + 1);
 	ft_strlcat(path, STR_DIR, PATH_MAX + 1);
 	ft_strlcat(path, arg, PATH_MAX + 1);
 }
