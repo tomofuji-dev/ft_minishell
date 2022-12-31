955219cced4cf126b5f4253a44df1f5d03c7b261
tfuji <tomofuji.dev@gmail.com>
2022-12-31 14:13:26 +0900

    modify ms_utils.c to embed macros

--- src/ms_utils.c
+++ src/ms_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tfujiwar <tfujiwar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 16:11:38 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/27 14:20:37 by tfujiwar         ###   ########.fr       */
+/*   Updated: 2022/12/31 14:10:54 by t.fuji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*free_and_return(void *malloc_obj)
 void	*print_err_set_status_return_null(char *str, int status)
 {
 	if (str != NULL)
-		ft_putendl_fd(str, 2);
+		ft_putendl_fd(str, STDERR_FILENO);
 	if (status >= 0)
 		g_shell.status = status;
 	return (NULL);
@@ -29,8 +29,8 @@ void	*print_err_set_status_return_null(char *str, int status)
 
 void	ms_init_fd(int fd[2])
 {
-	fd[0] = 0;
-	fd[1] = 1;
+	fd[0] = STDIN_FILENO;
+	fd[1] = STDOUT_FILENO;
 }
 
 bool	ms_is_directory(char *path)
