a9df2ff394e7517d3bfa2be7a482e2ce72c5b790
ykosaka <ykosaka@student.42tokyo.jp>
2023-01-09 21:31:41 +0900

    Modify parser_cmdnew (change order of assignment funcs)

--- src/ms_parser_cmdnew.c
+++ src/ms_parser_cmdnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms_parser_cmdnew.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: t.fuji <t.fuji@student.42.fr>              +#+  +:+       +#+        */
+/*   By: Yoshihiro Kosaka <ykosaka@student.42tok    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 14:28:00 by tfujiwar          #+#    #+#             */
-/*   Updated: 2022/12/31 12:21:28 by t.fuji           ###   ########.fr       */
+/*   Updated: 2023/01/09 20:14:08 by Yoshihiro K      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,17 @@ t_cmd	*ms_parser_cmdnew(t_token *token, size_t *idx)
 	cmd = (t_cmd *)ft_calloc(1, sizeof(t_cmd));
 	if (cmd == NULL)
 		return (NULL);
-	cmd->arg = ms_parser_cmdnew_arg(token, *idx);
-	if (cmd->arg == NULL)
-		return (ms_clear_cmd_and_return_null(cmd));
-	cmd->path = ms_getpath_cmd(cmd->arg[0]);
-	if (cmd->path == NULL)
-		return (ms_clear_cmd_and_return_null(cmd));
 	cmd->input = ms_parser_cmdnew_input(token, *idx);
 	if (cmd->input == NULL)
 		return (ms_clear_cmd_and_return_null(cmd));
 	cmd->output = ms_parser_cmdnew_output(token, *idx);
 	if (cmd->output == NULL)
+		return (ms_clear_cmd_and_return_null(cmd));
+	cmd->arg = ms_parser_cmdnew_arg(token, *idx);
+	if (cmd->arg == NULL)
+		return (ms_clear_cmd_and_return_null(cmd));
+	cmd->path = ms_getpath_cmd(cmd->arg[0]);
+	if (cmd->path == NULL)
 		return (ms_clear_cmd_and_return_null(cmd));
 	while (token[*idx].str != NULL && token[*idx].flag != FLAG_PIPE)
 		(*idx)++;
